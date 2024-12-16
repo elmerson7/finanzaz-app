b83a4a9c20e06bd96d37fac8ddc68b454793eeff
Elmerson <elmerson_350@hotmail.com>
2024-12-16 06:24:26 +0000

    feat: configure Bootstrap distribution

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import './assets/styles/main.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/styles/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
