18f2399bc354e9c19504925a6407bf3b4d6c6f3f
Elmerson <elmerson_350@hotmail.com>
2024-12-16 00:52:00 +0000

    feat: project structure, folders and files

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 v-if="isAuthenticated">Bienvenido al Dashboard</h1>
+    <LoginView v-else />
+    <!-- Si no está autenticado, muestra el Login en la misma URL -->
+  </div>
+</template>
+
+<script lang="ts">
+import { useAuthStore } from '@/stores/auth'
+import LoginView from './LoginView.vue'
+
+export default {
+  components: { LoginView },
+  setup() {
+    const authStore = useAuthStore()
+    return {
+      isAuthenticated: authStore.isAuthenticated,
+    }
+  },
+}
+</script>
